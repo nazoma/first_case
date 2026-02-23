6e004015ac3ce897932d456364a1083e7656b9f9
nazoma <shibahome.e.youkoso.77@gmail.com>
2026-02-23 22:08:11 +0900

    Delete first_case directory

--- first_case/script.js
+++
@@ -1,8 +0,0 @@
-function checkAnswer() {
-  const input = document.getElementById("answer").value;
-  if (input === "2") {
-    window.location.href = "stage2.html";
-  } else {
-    alert("違います");
-  }
-}
